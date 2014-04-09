1f87da3444a602a27dc59bba8ad75a058be5cf5a
unknown <banditdolphin@gmail.com>
2014-04-10 02:00:13 +0530

    layout change of signin and login

--- app/src/main/java/troubleshoot/app/Signup2.java
+++ app/src/main/java/troubleshoot/app/Signup2.java
@@ -52,6 +52,7 @@ public class Signup2 extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup2);
+
         emails = getIntent().getStringExtra("email");
         passws = getIntent().getStringExtra("pass");
         phones = getIntent().getStringExtra("phone");
