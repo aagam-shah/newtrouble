44cf16346b3c57cefedec549385177bb70ef8992
Aagam Shah <aagam94@gmail.com>
2014-04-17 22:07:57 +0530

    added abt us

--- app/src/main/java/troubleshoot/app/Abt_us.java
+++ app/src/main/java/troubleshoot/app/Abt_us.java
@@ -1,13 +1,7 @@
 package troubleshoot.app;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.text.Html;
-import android.text.method.LinkMovementMethod;
-import android.view.View;
-import android.widget.TextView;
 
 
 
@@ -21,26 +15,12 @@ public class Abt_us extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.abtus);
+//
+//        TextView textView =(TextView)findViewById(R.id.tvsite);
+//        textView.setClickable(true);
+//        textView.setMovementMethod(LinkMovementMethod.getInstance());
+//        String text = "<a href='http://www.ahmedabadmirror.com'> www.ahmedabadmirror.com </a>";
+//        textView.setText(Html.fromHtml(text));
 
-        TextView textView =(TextView)findViewById(R.id.tvsite);
-        textView.setClickable(true);
-        textView.setMovementMethod(LinkMovementMethod.getInstance());
-        String text = "<a href='http://www.ahmedabadmirror.com'> www.ahmedabadmirror.com </a>";
-        textView.setText(Html.fromHtml(text));
-
-        final TextView tv = (TextView)findViewById(R.id.tvnumber);
-        tv.setClickable(true);
-        
-        tv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                    Intent callIntent = new Intent(Intent.ACTION_CALL);
-                    callIntent.setData(Uri.parse("tel:+" + tv.getText().toString().trim()));
-
-                    startActivity(callIntent );
-            }
-
-        });
     }
 }
